33dba3ed82b05da221042cdd6add314bcf43c6ca
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-09-30 18:17:26 +0100

    making incremental function

--- basic_function.py
+++ basic_function.py
@@ -1,1 +1,8 @@
+def increment(x):
+    return x + 1
 
+#REQUIREMENTS
+# 1. Should return the input value incremented by 1
+# 2. Should handle negative numbers correctly
+# 3. Should execute in good time
+# 4. Should return an error if input is not a number
