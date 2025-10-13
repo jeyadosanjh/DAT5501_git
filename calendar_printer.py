5bf6cfdae074b605d6b2a665c0c321c333c5a075
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-13 08:26:09 +0100

    fixing calendar

--- calendar_printer.py
+++ calendar_printer.py
@@ -3,13 +3,13 @@ num_days = int(input("Enter number of days in the month (28-31): " ))
 start_day = int(input("Enter the starting day of the week (0=Sun, 1=Mon,...6=Sat): " ))
 
 print("S  M  T  W  T  F  S")
-#print initial dashes for days before the first
+#print initial spaces for days before the first
 for i in range(start_day):
-    print("-", end="  ")
+    print("  ", end="  ")
 #print the days
 for day in range(1, num_days+1):
-    print(f"{day:2}", end=" ")
-    #print newline after every 7 columns
+    print(f"{day:2}", end="  ")
+    #prints newline after every 7 columns
     if (day + start_day) % 7 == 0:
         print()
-print()  #final new line at the end of the month
+print()  # final new line at the end of the month
