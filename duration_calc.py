d2a8d070cae73dc11c872d927a69d5b7cfd212f9
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-20 11:30:07 +0100

    aadding new function using csv dates

--- duration_calc.py
+++ duration_calc.py
@@ -1,5 +1,16 @@
 import numpy as np
 import datetime
+import pandas as pd
+
+def past_days_csv():
+    df = pd.read_csv('random_dates.csv') #read the random dates CSV file
+    for x in df['date']:
+        date_convert = np.datetime64(x, 'D') #convert to numpy datetime64 object
+        today = np.datetime64('today', 'D') #get today's date in YYYY-MM-DD format
+        difference = today - date_convert #calculate difference in days
+        days_difference = difference.astype(int) #convert to integer
+        print(f"Date: {x}, Days from today: {days_difference}") #print the difference
+
 
 def difference_in_days(user_date):
     today = np.datetime64('today', 'D') #get today's date in YYYY-MM-DD format
@@ -13,3 +24,5 @@ if __name__ == "__main__":
     user_date = input("Enter a date in YYYY-MM-DD format:") #take user input
     days_difference = difference_in_days(user_date)
     print(f"The difference is {days_difference} days.") #print the difference
+    print("Calculating days difference for dates in random_dates.csv:")
+    past_days_csv()
