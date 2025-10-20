44a084f62c3d827c9c19d17b817d6340e8b47c1c
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-20 11:17:30 +0100

    fixing function

--- duration_calc.py
+++ duration_calc.py
@@ -1,10 +1,15 @@
 import numpy as np
 import datetime
 
-today = np.datetime64('today', 'D') #get today's date in YYYY-MM-DD format
-user_date = input("Enter a date in YYYY-MM-DD format:") #take user input
+def difference_in_days(user_date):
+    today = np.datetime64('today', 'D') #get today's date in YYYY-MM-DD format
 
-difference = np.datetime64(user_date, 'D') - today #calculate difference in days
-days_difference = difference.astype(int) #convert to integer
-days_difference = abs(days_difference) #get absolute value - no negative days
-print(f"The difference is {days_difference} days.") #print the difference
+    difference = np.datetime64(user_date, 'D') - today #calculate difference in days
+    days_difference = difference.astype(int) #convert to integer
+    days_difference = abs(days_difference) #get absolute value - no negative days
+    return days_difference
+
+if __name__ == "__main__":
+    user_date = input("Enter a date in YYYY-MM-DD format:") #take user input
+    days_difference = difference_in_days(user_date)
+    print(f"The difference is {days_difference} days.") #print the difference
