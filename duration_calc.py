077a38daec0024e84f86bf94e1b17de174f3006b
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-20 09:19:35 +0100

    duration calculation between today and a user-input date

--- duration_calc.py
+++ duration_calc.py
@@ -1,1 +1,10 @@
+import numpy as np
+import datetime
 
+today = np.datetime64('today', 'D') #get today's date in YYYY-MM-DD format
+user_date = input("Enter a date in YYYY-MM-DD format:") #take user input
+
+difference = np.datetime64(user_date, 'D') - today #calculate difference in days
+days_difference = difference.astype(int) #convert to integer
+days_difference = abs(days_difference) #get absolute value - no negative days
+print(f"The difference is {days_difference} days.") #print the difference
