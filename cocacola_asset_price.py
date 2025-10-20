f45eb8291f2d2ed826a5afc76926fcf3d2ed4a2f
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-20 12:25:08 +0100

    cleaning data and plotting percentage change graph

--- cocacola_asset_price.py
+++ cocacola_asset_price.py
@@ -6,6 +6,8 @@ from matplotlib.ticker import MaxNLocator
 #cleaning cocacola data
 df = pd.read_csv('cocacola_data.csv')
 df = df.drop(['Volume', 'Open', 'High', 'Low'], axis=1)  # remove unnecessary columns
+# converting 'Close/Last' to remove $ sign and convert to float
+df['Close/Last'] = df['Close/Last'].replace('[\$,]', '', regex=True).astype(float)
 
 #plotting closing price against date
 def plot_cocacola_data():
@@ -18,5 +20,18 @@ def plot_cocacola_data():
     plt.savefig("cocacola_closing_price.png")
     plt.show()
 
+#plotting percent change against date
+def plot_cocacola_percent_change():
+    df['Percent Change'] = df['Close/Last'].pct_change() * 100  #calculate percent change
+    plt.plot(pd.to_datetime(df['Date']), df['Percent Change'], label='CocaCola Percent Change', color='orange')
+    plt.xlabel('Date')
+    plt.ylabel('Percent Change (%)')
+    plt.title('CocaCola Asset Percent Change Over Time')
+    plt.legend()
+    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))  # show only whole numbers on y-axis - cleans up y axis
+    plt.savefig("cocacola_percent_change.png")
+    plt.show()
+
 if __name__ == "__main__":
     plot_cocacola_data()
+    plot_cocacola_percent_change()
