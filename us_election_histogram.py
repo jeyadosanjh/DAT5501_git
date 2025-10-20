efb1d53ddef66a8558a00a2f0e21814847c0ee70
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-10-20 12:47:26 +0100

    plotted histogram of fraction of votes and saved as png file

--- us_election_histogram.py
+++ us_election_histogram.py
@@ -1,1 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#plotting histograms of fraction of votes
+df = pd.read_csv('US-2016-primary.csv', sep=';')
+
+def plot_histograms():
+    plt.hist(df['fraction_votes'], bins=20)
+    plt.xlabel('Fraction of Votes')
+    plt.ylabel('Count')
+    plt.title('Histogram of Fraction of Votes (US 2016 Primary)')
+    plt.grid(axis='y', alpha=0.75)
+    plt.savefig('us_election_fraction_votes_histogram.png')
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_histograms()
